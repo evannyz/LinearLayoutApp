9bf4e0ba948632da035b95515aed68976ce888c4
evans <evanny.zapata@gmail.com>
2021-10-09 12:10:58 -0300

    Se crea segunda actividad para segundo ejercicio de Linear Layout

---
+++ app/src/main/java/com/example/ejerciciopdf/EjercicioDosPdf.java
@@ -0,0 +1,14 @@
+package com.example.ejerciciopdf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EjercicioDosPdf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio_dos_pdf);
+    }
+}
